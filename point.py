4384fd18e79f198b4e4e65ecd96348e9d3f5404f
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2021-02-08 23:11:01 +0200

    created point class with helper functions

---
+++ point.py
@@ -0,0 +1,19 @@
+import math
+
+
+class Point(object):
+
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+
+    def getX(self):
+        return self.X
+
+    def getY(self):
+        return self.Y
+
+    def distance(self, target_point):
+        dx = self.X - target_point.X
+        dy = self.Y - target_point.Y
+        return math.sqrt(dx**2 + dy**2)
